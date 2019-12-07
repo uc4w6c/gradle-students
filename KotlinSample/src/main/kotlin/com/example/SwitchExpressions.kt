eb6253176a70f60cccc8d646e6460af8cdbf4da5
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-07 12:27:05 +0900

    CompactNumberFormat

--- KotlinSample/src/main/kotlin/com/example/SwitchExpressions.kt
+++ KotlinSample/src/main/kotlin/com/example/SwitchExpressions.kt
@@ -1,9 +1,11 @@
 package com.example
 
 fun main(args: Array<String>) {
-    val s = "OCEAN"
+    // val s = "OCEAN"
+    val s = "ARIGATO"
     val releaseYear = when(s) {
         "ultra soul" -> "2001";
+        "BANZAI", "ARIGATO" -> "2004";
         "OCEAN" -> "2005";
         else -> "";
     }
