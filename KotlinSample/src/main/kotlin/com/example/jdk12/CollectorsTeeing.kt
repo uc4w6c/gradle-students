a03efdbf465b102e2bd7bbf442487ccd6ea1c855
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-07 23:41:02 +0900

    CollectorsTeeing

--- KotlinSample/src/main/kotlin/com/example/jdk12/CollectorsTeeing.kt
+++ KotlinSample/src/main/kotlin/com/example/jdk12/CollectorsTeeing.kt
@@ -1,4 +1,11 @@
 package com.example.jdk12
 
-class CollectorsTeeing {
+fun main() {
+    fun listToMap(list: List<String>): Map<String, Int> {
+        return mutableMapOf(list.joinToString(",") to list.count())
+    }
+    val list = mutableListOf("aaa", "", "bbb", "ccc")
+                    .filter { !it.isBlank() }
+                    .toList()
+    println(listToMap(list))
 }
